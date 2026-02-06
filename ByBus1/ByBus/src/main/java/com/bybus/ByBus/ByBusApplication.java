1206db446e0a97be1732d77237d326fbfd00324f
Thathsara Pramuditha <pramudithamunasinghe11@gmail.com>
2026-02-06 22:31:58 +0530

    add spring file

---
+++ ByBus1/ByBus/src/main/java/com/bybus/ByBus/ByBusApplication.java
@@ -0,0 +1,13 @@
+package com.bybus.ByBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByBusApplication.class, args);
+	}
+
+}
